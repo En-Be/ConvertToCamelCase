d4f9d64b4855ae0cd9504d3ac36c773388d43bdf
Nicholas <n3ttl3t@gmail.com>
2019-12-10 14:41:50 +0000

    first passing test

--- ConvertToCamelCase.cs
+++ ConvertToCamelCase.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Text;
 
 public static class Converter
 {
     public static string ToCamelCase(string arg)
     {
-        return "helloThereYou";
+        string result = "";
+        string[] splitArg = arg.Split('-');
+        for(int i = 0; i < splitArg.Length; i++)
+        {
+            StringBuilder sb = new StringBuilder(splitArg[i]);
+
+            if(i != 0)
+            {
+                sb[0] = char.ToUpper(sb[0]);
+            }
+            splitArg[i] = sb.ToString();
+        }
+        result = string.Join("", splitArg);
+        return result;
     }
 }
